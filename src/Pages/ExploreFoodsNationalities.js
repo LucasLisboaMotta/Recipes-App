2a67a612f87ec073613fb25f5010a9b10ae4e130
Lucas Lisboa <lucas@pop-os.localdomain>
2022-03-28 18:24:15 -0300

    requisito 10: completo, criada todas as paginas com e sem o header

---
+++ src/Pages/ExploreFoodsNationalities.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import Header from '../components/Header';
+
+export default function ExploreFoodsNationalities() {
+  return (
+    <div>
+      <Header />
+      ExploreFoodsNationalities
+    </div>
+  );
+}
--- src/Routes/Routes.js
+++ src/Routes/Routes.js
@@ -5,6 +5,18 @@ import Foods from '../Pages/Foods';
 import FoodsDetail from '../Pages/FoodsDetail';
 import FoodsInProgress from '../Pages/FoodsInProgress';
 import Drinks from '../Pages/Drinks';
+import DrinksDetail from '../Pages/DrinksDetail';
+import DrinksInProgress from '../Pages/DrinksInProgress';
+import Explore from '../Pages/Explore';
+import ExploreDrinks from '../Pages/ExploreDrinks';
+import ExploreFoods from '../Pages/ExploreFoods';
+import ExploreDrinksIngredients from '../Pages/ExploreDrinksIngredients';
+import ExploreFoodsIngredients from '../Pages/ExploreFoodsIngredients';
+import ExploreFoodsNationalities from '../Pages/ExploreFoodsNationalities';
+import Profile from '../Pages/Profile';
+import DoneRecipes from '../Pages/DoneRecipes';
+import FavoriteRecipes from '../Pages/FavoriteRecipes';
+import NotFound from '../Pages/NotFound';
 
 function Routes() {
   return (
@@ -17,23 +29,35 @@ function Routes() {
         <Route exact path="/foods/:id/in-progress" component={ FoodsInProgress } />
 
         <Route exact path="/drinks" component={ Drinks } />
-        <Route exact path="/drinks/:id" component={ About } />
-        <Route exact path="/drinks/:id/in-progress" component={ About } />
+        <Route exact path="/drinks/:id" component={ DrinksDetail } />
+        <Route exact path="/drinks/:id/in-progress" component={ DrinksInProgress } />
 
-        <Route exact path="/explore" component={ About } />
-        <Route exact path="/explore/foods" component={ About } />
-        <Route exact path="/explore/drinks" component={ About } />
-        <Route exact path="/explore/foods/ingredients" component={ About } />
-        <Route exact path="/explore/drinks/ingredients" component={ About } />
-        <Route exact path="/explore/foods/nationalities" component={ About } />
+        <Route exact path="/explore" component={ Explore } />
+        <Route exact path="/explore/foods" component={ ExploreFoods } />
+        <Route exact path="/explore/drinks" component={ ExploreDrinks } />
+        <Route
+          exact
+          path="/explore/foods/ingredients"
+          component={ ExploreFoodsIngredients }
+        />
+        <Route
+          exact
+          path="/explore/drinks/ingredients"
+          component={ ExploreDrinksIngredients }
+        />
+        <Route
+          exact
+          path="/explore/foods/nationalities"
+          component={ ExploreFoodsNationalities }
+        />
 
-        <Route exact path="/profile" component={ About } />
+        <Route exact path="/profile" component={ Profile } />
 
-        <Route exact path="/done-recipes" component={ About } />
+        <Route exact path="/done-recipes" component={ DoneRecipes } />
 
-        <Route exact path="/favorite-recipes" component={ About } />
+        <Route exact path="/favorite-recipes" component={ FavoriteRecipes } />
 
-        <Route exact path="*" component={ About } />
+        <Route exact path="*" component={ NotFound } />
       </Switch>
     </BrowserRouter>
   );
