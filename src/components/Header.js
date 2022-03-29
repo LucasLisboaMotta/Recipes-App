e58b1620212b7df1efc2d4df9e1fb7ee169ba80d
Lucas Lisboa <lucas@pop-os.localdomain>
2022-03-29 01:31:22 -0300

    header: refatorando, alterando o icone de busca

--- src/components/Header.js
+++ src/components/Header.js
@@ -7,6 +7,15 @@ import SearchIcon from '../images/searchIcon.svg';
 export default function Header({ page, handleSearch }) {
   const [hasSearchClick, setSearchClick] = useState(false);
 
+  const SearchButton = (
+    <button
+      type="button"
+      onClick={ () => setSearchClick(!hasSearchClick) }
+    >
+      <img src={ SearchIcon } alt="search icon" data-testid="search-top-btn" />
+    </button>
+  );
+
   return (
     <header>
       <Link to="/profile">
@@ -14,13 +23,7 @@ export default function Header({ page, handleSearch }) {
       </Link>
       <h1 data-testid="page-title">{page}</h1>
       {hasSearchClick && <input data-testid="search-input" />}
-      <button
-        type="button"
-        onClick={ () => setSearchClick(!hasSearchClick) }
-      >
-        {handleSearch
-        && <img src={ SearchIcon } alt="search icon" data-testid="search-top-btn" />}
-      </button>
+      {handleSearch && SearchButton }
     </header>
   );
 }
@@ -29,20 +32,3 @@ Header.propTypes = {
   page: PropTypes.string.isRequired,
   handleSearch: PropTypes.bool.isRequired,
 };
-
-// - Não tem header na tela de login
-// - O header tem os ícones corretos na tela de principal de receitas de comidas
-// - O header tem os ícones corretos na tela de principal de receitas de bebidas
-// - Não tem header na tela de detalhes de uma receita de comida
-// - Não tem header na tela de detalhes de uma receita de bebida
-// - Não tem header na tela de receita em progresso de comida
-// - Não tem header na tela de receita em progresso de bebida
-// - O header tem os ícones corretos na tela de explorar
-// - O header tem os ícones corretos na tela de explorar comidas
-// - O header tem os ícones corretos na tela de explorar bebidas
-// - O header tem os ícones corretos na tela de explorar comidas por ingrediente
-// - O header tem os ícones corretos na tela de explorar bebidas por ingrediente
-// - O header tem os ícones corretos na tela de explorar comidas por nacionalidade
-// - O header tem os ícones corretos na tela de perfil
-// - O header tem os ícones corretos na tela de receitas feitas
-// - O header tem os ícones corretos na tela de receitas favoritas
