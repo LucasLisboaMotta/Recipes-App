17da3c0e9abdaaa8542b61d8786986c14ec7f83f
Lucas Lisboa <lucas@pop-os.localdomain>
2022-03-29 16:31:45 -0300

    tela de busca: em andamento, finalizado cards de renderização

--- src/components/Cards.js
+++ src/components/Cards.js
@@ -1,7 +1,28 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
-export default function Cards() {
+export default function Cards({ page, api }) {
+  const keys = {
+    Foods: { name: 'strMeal', img: 'strMealThumb' },
+    Drinks: { name: 'strDrink', img: 'strDrinkThumb' },
+  };
   return (
-    <div>Cards</div>
+    <div>
+      { api.map((element, index) => (
+        <div key={ index } data-testid={ `${index}-recipe-card` }>
+          <h2 data-testid={ `${index}-card-name` }>{element[keys[page].name]}</h2>
+          <img
+            src={ element[keys[page].img] }
+            alt={ element[keys[page].name] }
+            data-testid={ `${index}-card-img` }
+          />
+        </div>
+      ))}
+    </div>
   );
 }
+
+Cards.propTypes = {
+  page: PropTypes.string.isRequired,
+  api: PropTypes.arrayOf(PropTypes.object).isRequired,
+};
