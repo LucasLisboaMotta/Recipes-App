a6348c4475b3a95fbd1b7bd91c0788818efdc01d
matheus-linux <matheusjcx@gmail.com>
2022-03-29 15:55:13 -0300

    Footer, finalizado, footer fixo nas páginas requisitadas

--- src/Pages/ExploreFoodsIngredients.js
+++ src/Pages/ExploreFoodsIngredients.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import Header from '../components/Header';
+import Footer from '../components/Footer';
 
 export default function ExploreFoodsIngredients() {
   return (
     <div>
       <Header page="Explore Ingredients" handleSearch={ false } />
       ExploreFoodsIngredients
+      <Footer />
     </div>
   );
 }
