ce98f6fe25439d8bd587a3e272c6d1ffec0887dc
Lucas Lisboa <lucas@pop-os.localdomain>
2022-03-29 18:29:22 -0300

    tela de busca: completa, implementada a barra de busca com sucesso

--- src/components/Cards.js
+++ src/components/Cards.js
@@ -1,22 +1,31 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import PropTypes from 'prop-types';
+import { Link } from 'react-router-dom';
+import context from '../context/Context';
 
-export default function Cards({ page, api }) {
+export default function Cards({ page }) {
   const keys = {
-    Foods: { name: 'strMeal', img: 'strMealThumb' },
-    Drinks: { name: 'strDrink', img: 'strDrinkThumb' },
+    Foods: { name: 'strMeal', img: 'strMealThumb', id: 'idMeal' },
+    Drinks: { name: 'strDrink', img: 'strDrinkThumb', id: 'idDrink' },
   };
+  const { state: { foods } } = useContext(context);
   return (
     <div>
-      { api.map((element, index) => (
-        <div key={ index } data-testid={ `${index}-recipe-card` }>
-          <h2 data-testid={ `${index}-card-name` }>{element[keys[page].name]}</h2>
-          <img
-            src={ element[keys[page].img] }
-            alt={ element[keys[page].name] }
-            data-testid={ `${index}-card-img` }
-          />
-        </div>
+      { foods.filter((_, index) => {
+        const MAXIMUM_ARRAY_LENGTH = 12;
+        if (index < MAXIMUM_ARRAY_LENGTH) return true;
+        return false;
+      }).map((element, index) => (
+        <Link key={ index } to={ `/${page.toLowerCase()}/${element[keys[page].id]}` }>
+          <div data-testid={ `${index}-recipe-card` }>
+            <h2 data-testid={ `${index}-card-name` }>{element[keys[page].name]}</h2>
+            <img
+              src={ element[keys[page].img] }
+              alt={ element[keys[page].name] }
+              data-testid={ `${index}-card-img` }
+            />
+          </div>
+        </Link>
       ))}
     </div>
   );
@@ -24,5 +33,4 @@ export default function Cards({ page, api }) {
 
 Cards.propTypes = {
   page: PropTypes.string.isRequired,
-  api: PropTypes.arrayOf(PropTypes.object).isRequired,
 };
--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,10 +1,13 @@
 import PropTypes from 'prop-types';
+import { useHistory } from 'react-router-dom';
 import React, { useContext, useState } from 'react';
 import context from '../context/Context';
+import Cards from './Cards';
 
 function SearchBar({ page }) {
   const [getBarText, setBarText] = useState();
   const [getBtns, setBtns] = useState();
+  const [renderCards, setRenderCards] = useState(false);
   const { setState } = useContext(context);
   const buttonsObj = {
     igredientBtn: true,
@@ -45,70 +48,79 @@ function SearchBar({ page }) {
     enableButton({ ...btnsObj, [event.target.name]: true });
   };
 
-  const issueAlert = () => {
-    if (getBarText.length > 1 && getBtns === 'first-letter') {
-      global.alert('Your search must have only 1 (one) character');
-    }
-  };
-
+  const history = useHistory();
   const handleSubmit = async (event) => {
     event.preventDefault();
-    issueAlert();
+    setRenderCards(false);
+    if (getBarText.length > 1 && getBtns === 'first-letter') {
+      return global.alert('Your search must have only 1 (one) character');
+    }
     const result = await fetch(getURL(page, getBtns, getBarText));
     const data = await result.json();
-    setState({ foods: Object.values(data) });
+    const foods = Object.values(data)[0];
+    setState({ foods });
+    if (foods === null) {
+      global.alert('Sorry, we haven\'t found any recipes for these filters.');
+    } else if (foods.length === 1) {
+      const keys = { Foods: 'idMeal', Drinks: 'idDrink' };
+      history.push(`/${page.toLowerCase()}/${foods[0][keys[page]]}`);
+    } else {
+      setRenderCards(true);
+    }
   };
-
   return (
-    <form onSubmit={ handleSubmit }>
-      <label htmlFor="search-bar">
-        <input
-          id="search-bar"
-          data-testid="search-input"
-          name="searchBar"
-          onChange={ handleSearchBar }
-        />
-      </label>
-      <label htmlFor="ingredient-button">
-        <input
-          id="ingredient-button"
-          type="radio"
-          data-testid="ingredient-search-radio"
-          name="ingredient"
-          checked={ isEnableButton.ingredient }
-          onChange={ handleRadioButtons }
-        />
-        Ingredient
-      </label>
-      <label htmlFor="name-btn">
-        <input
-          id="name-btn"
-          type="radio"
-          data-testid="name-search-radio"
-          name="name"
-          checked={ isEnableButton.name }
-          onChange={ handleRadioButtons }
-        />
-        Name
-      </label>
-      <label htmlFor="first-letter-btn">
-        <input
-          if="first-letter-btn"
-          type="radio"
-          data-testid="first-letter-search-radio"
-          name="first-letter"
-          checked={ isEnableButton['first-letter'] }
-          onChange={ handleRadioButtons }
-        />
-        First Letter
-      </label>
-      <button
-        type="submit"
-        data-testid="exec-search-btn"
-      >
-        Search
-      </button>
-    </form>
+    <>
+      <form onSubmit={ handleSubmit }>
+        <label htmlFor="search-bar">
+          <input
+            id="search-bar"
+            data-testid="search-input"
+            name="searchBar"
+            onChange={ handleSearchBar }
+          />
+        </label>
+        <label htmlFor="ingredient-button">
+          <input
+            id="ingredient-button"
+            type="radio"
+            data-testid="ingredient-search-radio"
+            name="ingredient"
+            checked={ isEnableButton.ingredient }
+            onChange={ handleRadioButtons }
+          />
+          Ingredient
+        </label>
+        <label htmlFor="name-btn">
+          <input
+            id="name-btn"
+            type="radio"
+            data-testid="name-search-radio"
+            name="name"
+            checked={ isEnableButton.name }
+            onChange={ handleRadioButtons }
+          />
+          Name
+        </label>
+        <label htmlFor="first-letter-btn">
+          <input
+            id="first-letter-btn"
+            type="radio"
+            data-testid="first-letter-search-radio"
+            name="first-letter"
+            checked={ isEnableButton['first-letter'] }
+            onChange={ handleRadioButtons }
+          />
+          First Letter
+        </label>
+        <button
+          type="submit"
+          data-testid="exec-search-btn"
+        >
+          Search
+        </button>
+      </form>
+      {renderCards && <Cards page={ page } />}
+    </>
   );
 }
 
