efd0ea7a6247de9f42b0cc65fab6055576802fe3
Álvaro Ramos <alvaroramos2222@hotmail.com>
2022-03-30 15:32:25 -0300

    tela principal, em andamento, implementei os botoes de filtro

--- src/Pages/Drinks.js
+++ src/Pages/Drinks.js
@@ -1,14 +1,43 @@
-import React from 'react';
+import React, { useCallback, useState, useEffect } from 'react';
 import Header from '../components/Header';
 import Footer from '../components/Footer';
 import '../styles/drinks.css';
 
-export default function Drinks() {
+function Drinks() {
+  const [categories, setCategories] = useState();
+  const getCategories = useCallback(async () => {
+    const result = await fetch('https://www.thecocktaildb.com/api/json/v1/1/list.php?c=list');
+    const data = await result.json();
+    setCategories(data.drinks);
+  }, [setCategories]);
+
+  useEffect(() => { getCategories(); }, [getCategories]);
+  if (!categories) {
+    return <p>Carregando...</p>;
+  }
+
+  const filteredCategories = categories.filter((_, index) => {
+    const MAXIMUM_ARRAY_LENGTH = 5;
+    if (index < MAXIMUM_ARRAY_LENGTH) return true;
+    return false;
+  });
+
   return (
     <div className="drinks">
       <Header page="Drinks" handleSearch />
       Drinks
+      {filteredCategories.map(({ strCategory }) => (
+        <button
+          type="button"
+          key={ strCategory }
+          data-testid={ `${strCategory}-category-filter` }
+        >
+          { strCategory }
+        </button>
+      ))}
       <Footer />
     </div>
   );
 }
+
+export default Drinks;
--- src/Pages/Foods.js
+++ src/Pages/Foods.js
@@ -1,14 +1,43 @@
-import React from 'react';
+import React, { useCallback, useState, useEffect } from 'react';
 import Header from '../components/Header';
 import Footer from '../components/Footer';
 import '../styles/foods.css';
 
-export default function Foods() {
+function Foods() {
+  const [categories, setCategories] = useState();
+  const getCategories = useCallback(async () => {
+    const result = await fetch('https://www.themealdb.com/api/json/v1/1/list.php?c=list');
+    const data = await result.json();
+    setCategories(data.meals);
+  }, [setCategories]);
+
+  useEffect(() => { getCategories(); }, [getCategories]);
+  if (!categories) {
+    return <p>Carregando...</p>;
+  }
+
+  const filteredCategories = categories.filter((_, index) => {
+    const MAXIMUM_ARRAY_LENGTH = 5;
+    if (index < MAXIMUM_ARRAY_LENGTH) return true;
+    return false;
+  });
+
   return (
     <div className="foods">
       <Header page="Foods" handleSearch />
       Foods
+      {filteredCategories.map(({ strCategory }) => (
+        <button
+          type="button"
+          key={ strCategory }
+          data-testid={ `${strCategory}-category-filter` }
+        >
+          { strCategory }
+        </button>
+      ))}
       <Footer />
     </div>
   );
 }
+
+export default Foods;
