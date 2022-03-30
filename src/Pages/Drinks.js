0dc9585f6b0a9cc688ae0331f9520f0b36a41540
Lucas Lisboa <lucas@pop-os.localdomain>
2022-03-30 15:06:46 -0300

    tela principal: em andamento, reposicionando o componente card e adicionando um fetch inicial

--- src/Pages/Drinks.js
+++ src/Pages/Drinks.js
@@ -1,13 +1,25 @@
-import React from 'react';
+import React, { useEffect, useContext } from 'react';
 import Header from '../components/Header';
 import Footer from '../components/Footer';
-import '../styles/drinks.css';
+import context from '../context/Context';
+import '../styles/foods.css';
+import Cards from '../components/Cards';
 
-export default function Drinks() {
+export default function Foods() {
+  const { setState } = useContext(context);
+  useEffect(() => {
+    const getApi = async () => {
+      const URL = 'https://www.thecocktaildb.com/api/json/v1/1/filter.php?c=Cocktail';
+      const getInicialRecipes = await fetch(URL);
+      const resolveInicialRecipes = await getInicialRecipes.json();
+      setState({ foods: resolveInicialRecipes.drinks });
+    };
+    getApi();
+  }, [setState]);
   return (
     <div className="drinks">
       <Header page="Drinks" handleSearch />
-      Drinks
+      <Cards page="Drinks" />
       <Footer />
     </div>
   );
--- src/Pages/Foods.js
+++ src/Pages/Foods.js
@@ -1,13 +1,25 @@
-import React from 'react';
+import React, { useEffect, useContext } from 'react';
 import Header from '../components/Header';
 import Footer from '../components/Footer';
+import context from '../context/Context';
 import '../styles/foods.css';
+import Cards from '../components/Cards';
 
 export default function Foods() {
+  const { setState } = useContext(context);
+  useEffect(() => {
+    const getApi = async () => {
+      const URL = 'https://www.themealdb.com/api/json/v1/1/filter.php?c=Beef';
+      const getInicialRecipes = await fetch(URL);
+      const resolveInicialRecipes = await getInicialRecipes.json();
+      setState({ foods: resolveInicialRecipes.meals });
+    };
+    getApi();
+  }, [setState]);
   return (
     <div className="foods">
       <Header page="Foods" handleSearch />
-      Foods
+      <Cards page="Foods" />
       <Footer />
     </div>
   );
